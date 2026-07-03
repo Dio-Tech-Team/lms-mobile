dd43925ce5b9d2577f70bd3a4374944fa53897d0
blanzadeannsamuel-crypto <blanzadeannsamuel@gmail.com>
2026-07-03 15:49:42 +0800

    updated reload and login issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_providers.dart';
-import '../users/login_page.dart';
-import '../screentabs/homepage.dart';
+import 'users/login_page.dart';
+import 'screentabs/homepage.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()..tryAutoLogin()),
       ],
       child: const MyApp(),
     ),
@@ -25,10 +25,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LoginPage(),
+      home: const AuthWrapper(),
       routes: {
         '/home': (context) => const HomePage(),
       },
     );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = context.watch<AuthProvider>();
+
+    if (authProvider.isInitializing) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator()),
+      );
+    } return authProvider.isLoggedIn ? const HomePage() : const LoginPage();
   }
 }
